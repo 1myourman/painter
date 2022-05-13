ea5d725da8f7170aa9e934e75fa44e9cfcd32ff1
Alice <59229770+1myourman@users.noreply.github.com>
2022-05-13 22:37:47 +0900

    Delete codingtests directory

--- codingtests/quarter.js
+++
@@ -1,8 +0,0 @@
-
-const getQuarter = (months) => {
-	if (months / 3 <= 1) return 1;
-	else if (months / 3 <= 2) return 2;
-	else if (months / 3 <= 3) return 3;
-	else if (months / 3 <= 4) return 4;
-	else return "Please enter valid number";
-}
